b853907d3bef56f56ed550dc1d40fa4fd9274ce1
GuiMonLopes <guilherme.monteiro1@gmail.com>
2021-12-12 13:17:13 -0300

    Declarando Variaveis

---
+++ FundamentosJavaScript/type-number.js
@@ -0,0 +1,25 @@
+// tipo number
+
+const meuNumero = 3;
+
+const num1 = 1;
+const num2 = 2;
+
+const operaçãoMatematica = num1 * num2;
+
+console.log(operaçãoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFLutuanteSemZero = .5;
+
+const operaçãoMatematicaPontoFlutuante = num1 / numeroPontoFlutuante;
+
+console.log(operaçãoMatematicaPontoFlutuante);
+
+//NaN -> Not a Number
+
+const alura = "Alura";
+
+console.log(alura * num1);
