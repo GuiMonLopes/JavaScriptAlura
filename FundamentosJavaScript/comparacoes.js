597e75ec8180f6fb9dd0263745c2654b0d999092
GuiMonLopes <guilherme.monteiro1@gmail.com>
2021-12-20 22:21:02 -0300

    Inicio dos estudo com comparadores no JavaScript

---
+++ FundamentosJavaScript/comparacoes.js
@@ -0,0 +1,22 @@
+// ==(comparação implicita)
+
+const numeros = 5;
+
+const texto = "5";
+
+console.log(numeros === texto);
+
+//typeof
+
+console.log(typeof numeros);
+console.log(typeof texto);
+
+
+//== só compara o valor
+
+// === compara o valor e o tipo de dados
+
+// conversão explicita
+
+Number()
+String()
