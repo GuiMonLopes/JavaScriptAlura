02f1dce1dfff1847749b8ceafc5db1fd4661b025
GuiMonLopes <guilherme.monteiro1@gmail.com>
2021-12-21 21:37:37 -0300

    FInalizazçao dos estudos de operadores

---
+++ FundamentosJavaScript/template-string.js
@@ -0,0 +1,10 @@
+const nome = "Guilherme";
+const idade = 2021 - 1992;
+const cidadeDeNascimento = "São Paulo"
+// const apresentacao = "Meu nome é "+nome + ", minha idade é "+idade+ " , nasci na cidade de "+cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade}, nasci na cidade de ${cidadeDeNascimento}`
+
+
+console.log(apresentacao)
+
