712c83a5ca120751c25e4e4371bbb84bda7c5bc3
GuiMonLopes <guilherme.monteiro1@gmail.com>
2022-02-10 21:48:52 -0300

    Alterando Arrays

--- FundamentosJavaScriptArrays/alternandoArrays.js
+++ FundamentosJavaScriptArrays/alternandoArrays.js
@@ -19,3 +19,34 @@
 
 // let media = (notas[0]+notas[1]+notas[2]+notas[3])/notas.length
 // console.log(media)
+
+// const nomes = ['Maria', 'Carlos', 'Eduardo', 'Samanta', 'André', 'Fernanda']
+
+// const sala1 =nomes.slice(0,nomes.length/2)
+// const sala2 = nomes.slice(nomes.length/2)
+
+// console.log(`Alunos da sala 1: ${sala1}`)
+// console.log(`Alunos da sala 2: ${sala2}`)
+
+// const listaChamada = ['Maria', 'Carlos', 'Eduardo', 'Samanta', 'André', 'Fernanda']
+
+// // listaChamada.splice(1, 2, 'Rodrigo')
+
+// listaChamada.splice(2, 0, 'Rodrigo')
+
+// console.log(listaChamada)
+
+
+// const salaDePython = ['Maria', 'Carlos', 'Eduardo']
+// const salaDeJavaScript = ['Samanta', 'André', 'Fernanda']
+
+// const salaConcat = salaDePython.concat(salaDeJavaScript)
+
+// console.log(salaConcat)
+
+const alunos = ['Maria', 'Carlos', 'Eduardo']
+const mediasAlunos = [10,9,8]
+
+let listaDeNotasEAlunos=[alunos, mediasAlunos]
+
+console.log(listaDeNotasEAlunos)
