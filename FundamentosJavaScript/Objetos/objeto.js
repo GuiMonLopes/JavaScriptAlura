554045cb9869c020cc794ed121590095b67fe4ca
GuiMonLopes <guilherme.monteiro1@gmail.com>
2022-03-12 11:14:33 -0300

    Inicio dos estudos do Objeto

---
+++ FundamentosJavaScript/Objetos/objeto.js
@@ -0,0 +1,10 @@
+const listaCPF= ["111111111", "985412154646", "451151684651"]
+
+const cliente = ["nome", "Andre", "idade", 36]
+
+const clientes = {
+    nome:"Andre",
+    idade: 36,
+    cpf: "1234567925",
+    email:"andre@email.com"
+}
---
+++ FundamentosJavaScript/Objetos/printInfos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome:"Andre",
+    idade: 36,
+    cpf: "1234567925",
+    email:"andre@email.com"
+}
+
+console.log(`Meu nome é ${cliente.nome} e tem ${cliente.idade} anos.`)
+console.log(cliente.cpf.substring(0,3))
