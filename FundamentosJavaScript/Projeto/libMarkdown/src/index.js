99b79b4302cec22368d8a156be5e0bde4d2dae84
GuiMonLopes <guilherme.monteiro1@gmail.com>
2022-05-30 21:55:38 -0300

    Regex

---
+++ FundamentosJavaScript/Projeto/libMarkdown/src/index.js
@@ -0,0 +1,33 @@
+import chalk from 'chalk';
+import * as fs from 'node:fs';
+
+
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados=[];
+    let temp;
+
+    while((temp = regex.exec(texto)) !== null){
+        arrayResultados.push({[temp[1]]:temp[2]});
+    }
+
+    return arrayResultados.length === 0 ? 'Não há links': arrayResultados ;
+}
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Não há arquivos no Path'));
+}
+//async/await
+
+export async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        return extraiLinks(texto);
+    }catch(e){
+        trataErro(e);
+    }finally{
+        console.log(chalk.yellow("\nOperação Finalizada!"));
+    }
+}
