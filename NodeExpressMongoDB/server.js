d722ef20ec944812bba63005c7d16c1e2450c82d
GuiMonLopes <guilherme.monteiro1@gmail.com>
2022-09-28 17:23:15 -0300

    Inicio dos estudos com Express

---
+++ NodeExpressMongoDB/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const port = 3000;
+
+const route= {
+    '/': "Curso de Node",
+    '/livros':"Entrei na pagina de livros",
+    '/autores': "Pagina de autores",
+    '/editora': "Pagina de editoras",
+    '/sobre': "Informação sobre o projeto"
+};
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(route[req.url]);
+});
+
+server.listen(port, ()=>{
+    console.log(`Servidor escutando em http://localhost:${port}`);
+});
