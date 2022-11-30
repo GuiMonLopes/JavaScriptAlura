786095ec5ed53a4f84ae535f90d8da556bd84e8d
GuiMonLopes <guilherme.monteiro1@gmail.com>
2022-11-30 20:40:52 -0300

    Criando a rota de post livros

--- NodeExpressMongoDB/src/app.js
+++ NodeExpressMongoDB/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {id:1, "titulo":"Senhor dos aneis"},
     {id:2, "titulo":"Guerra dos Tronos"}
@@ -13,6 +15,11 @@ app.get('/',(req,resp)=>{
 
 app.get('/livros',(req,resp)=>{
     resp.status(200).json(livros)
+});
+
+app.post('/livros', (req,resp)=>{
+    livros.push(req.body);
+    resp.status(201).send('Livros foi cadastrado com sucesso.');
 });
 
 export default app
