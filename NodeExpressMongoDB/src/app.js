59662c2d6a62c531a6d7ad919c8112668a0eadee
GuiMonLopes <guilherme.monteiro1@gmail.com>
2023-01-09 09:14:31 -0300

    Rotas

--- NodeExpressMongoDB/src/app.js
+++ NodeExpressMongoDB/src/app.js
@@ -17,9 +17,31 @@ app.get('/livros',(req,resp)=>{
     resp.status(200).json(livros)
 });
 
+app.get('/livros/:id', (req,resp)=>{
+    let index = buscaLivro(req.params.id);
+    resp.json(livros[index])
+})
+
 app.post('/livros', (req,resp)=>{
     livros.push(req.body);
     resp.status(201).send('Livros foi cadastrado com sucesso.');
 });
+
+app.put('/livros/:id', (req,resp)=>{
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    resp.json(livros)
+})
+
+app.delete('/livros/:id', (req,resp)=>{
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index,1);
+    resp.send(`Livro ${id} apagado com sucesso!`);
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro =>livro.id == id)
+}
 
 export default app
