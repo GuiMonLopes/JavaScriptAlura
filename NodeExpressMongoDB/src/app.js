282a86f69f3f9a4f86c0828ebc96d5244dfc7fe4
GuiMonLopes <guilherme.monteiro1@gmail.com>
2023-01-12 20:01:27 -0300

    Finalizando as rotas

--- NodeExpressMongoDB/src/app.js
+++ NodeExpressMongoDB/src/app.js
@@ -1,51 +1,14 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import livros from "./models/Livro.js"
 import routes from "./routes/index.js"
 
-
 db.on("error", console.log.bind(console, 'Erro de conexão'));
-db.once("open", ()=>{
+db.once("open", () => {
     console.log('Conexão com o banco feita com sucesso.')
 });
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
-
-// const livros = [
-//     {id:1, "titulo":"Senhor dos aneis"},
-//     {id:2, "titulo":"Guerra dos Tronos"}
-// ];
-
-
-app.get('/livros/:id', (req,resp)=>{
-    let index = buscaLivro(req.params.id);
-    resp.json(livros[index])
-})
-
-app.post('/livros', (req,resp)=>{
-    livros.push(req.body);
-    resp.status(201).send('Livros foi cadastrado com sucesso.');
-});
-
-app.put('/livros/:id', (req,resp)=>{
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    resp.json(livros)
-})
-
-app.delete('/livros/:id', (req,resp)=>{
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index,1);
-    resp.send(`Livro ${id} apagado com sucesso!`);
-})
-
-function buscaLivro(id){
-    return livros.findIndex(livro =>livro.id == id)
-}
 
 export default app
--- NodeExpressMongoDB/src/controllers/livrosControllers.js
+++ NodeExpressMongoDB/src/controllers/livrosControllers.js
@@ -1,11 +1,57 @@
 import livros from "../models/Livro.js"
 
-class LivroController{
-    static listarLivros = (req, resp) =>{
-        livros.find((err, livros)=>{
+class LivroController {
+    static listarLivros = (req, resp) => {
+        livros.find((err, livros) => {
             resp.status(200).json(livros)
-        })  
+        })
     };
+
+    static listarLivroById = (req, resp) => {
+        const { id } = req.params;
+        livros.findById(id, (err, livro) => {
+            if (err) {
+                resp.status(400).send({ message: err.message });
+            } else {
+                resp.status(200).send(livro);
+            }
+        });
+    };
+
+    static cadastrarLivro = (req, resp) => {
+        let livro = new livros(req.body);
+        livro.save((err) => {
+            if (err) {
+                resp.status(500).send({ message: `${err.message} - falha ao cadastrar livro.` });
+            } else {
+                resp.status(201).send(livro.toJSON());
+            }
+        })
+    };
+
+    static atualizarLivro = (req, resp) => {
+        const { id } = req.params;
+        livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+            if (!err) {
+                resp.status(200).send({ message: `Livro atualizado com sucesso` });
+            } else {
+                resp.status(500).send({ message: err.message });
+            }
+        });
+    }
+
+    static excluirLivro = (req, resp) => {
+        const { id } = req.params;
+        livros.findByIdAndDelete(id, (err) => {
+            if (!err) {
+                resp.status(200).send({ message: "Livro excluido com sucesso" });
+            } else {
+                resp.status(500).send({ message: err.message });
+            }
+        })
+
+    }
+
 };
 
 export default LivroController
