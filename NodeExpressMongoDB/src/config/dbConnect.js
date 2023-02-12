6e496498814409c43795bb4152a1c168614b5dba
GuiMonLopes <guilherme.monteiro1@gmail.com>
2023-02-12 14:31:47 -0300

    fim do projeto

--- NodeExpressMongoDB/src/config/dbConnect.js
+++ NodeExpressMongoDB/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://alura:@nodeexpress.md0zdvt.mongodb.net/alura-node");
+mongoose.connect("mongodb+srv://alura:mongodb1020@nodeexpress.md0zdvt.mongodb.net/alura-node");
 mongoose.set('strictQuery', true);
 
 let db = mongoose.connection;
